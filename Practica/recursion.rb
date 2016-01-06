7bbf225910927dada203aa48e3818df329858417
katusiky <josedeleon.ka@gmail.com>
2016-01-06 01:48:20 -0430

    change function name

--- Practica/recursion.rb
+++ Practica/recursion.rb
@@ -1,9 +1,9 @@
-def my_factorial number
+def myFactorial number
 	if number > 1
-		number *  my_factorial(number-1)
+		number *  myFactorial(number-1)
 	else
 		1
 	end
 end
 
-puts my_factorial(1)
+puts myFactorial(1)
